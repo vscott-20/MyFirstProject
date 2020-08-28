4c91feb184beb1a3b8ac1d0005e47356a933b710
vscott-20 <70227709+vscott-20@users.noreply.github.com>
2020-08-28 11:46:40 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,15 @@
+/*
+Viatris Scott
+August 31, 2020
+Lab 1 Itroduction to C++ Programming
+This lab is to serve as a demonstraction of my ability to download a project from GitHub, complie and run the project, upload a project to GitHub, and submit a project to Google Classroom.
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!";
+	std::cout << "Favorite Color is Purple";
+	return 0;
+}
